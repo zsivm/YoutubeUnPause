429f0995f16683356df3c24500ce09b1fac4abaa
Mihael Zsivics <zsivics.mihael@gmail.com>
2020-11-14 17:54:53 +0100

    fixed previous version, style-none is not working. This version removes the dom element

--- test/buttonListener.js
+++ test/buttonListener.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var okButton = document.getElementById('button');
+    var okButton = document.getElementById('button1');
     okButton.addEventListener('click', function() {
-        console.log("button was clicked");
+        //console.log("button was clicked");
+        let oButton = document.createElement("BUTTON");
+        oButton.innerHTML = "NEW BUTTON";
+        oButton.id="button";
+        oButton.classList.add("style-scope", "yt-button-renderer", "style-blue-text", "size-default");
+        document.body.appendChild(oButton);
       });
 }, false);
