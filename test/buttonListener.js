16497081a25ef966151f677a5ea9621b916c06f9
Mihael Zsivics <zsivics.mihael@gmail.com>
2020-11-15 20:28:04 +0100

    created version 1.1, now works without issues

--- test/buttonListener.js
+++ test/buttonListener.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var okButton = document.getElementById('button1');
     okButton.addEventListener('click', function() {
-        //console.log("button was clicked");
         let oButton = document.createElement("BUTTON");
         oButton.innerHTML = "NEW BUTTON";
         oButton.id="button";
