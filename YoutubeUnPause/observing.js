ff4077df947eb265b52e41851b7c3c1760d1cf1a
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-06-27 22:34:24 +0200

    created new branch which utilises the mutationObserver, live testing is required

---
+++ YoutubeUnPause/observing.js
@@ -0,0 +1,24 @@
+const elementToObserve = document.querySelector("ytd-popup-container");
+
+const observer = new MutationObserver(() => {
+    console.log("yt-popup-container got a child element");
+    observer.disconnect();
+    removeAllChildNodes(elementToObserve);
+    console.log("removed all children from yt-popup-container");
+    // todo: emulate click required?
+    this.activateObserver();
+    console.log("observer is active again");
+});
+
+this.activateObserver();
+console.log("Observer is watching!");
+
+function removeAllChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function activateObserver() {
+    observer.observe(elementToObserve, {subtree: true, childList: true});
+}
