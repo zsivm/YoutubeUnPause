a620f8801c8680d2f9c2592a66a980cecf6fd9ea
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-06-29 10:42:02 +0200

    changed message

--- YoutubeUnPause/observing.js
+++ YoutubeUnPause/observing.js
@@ -4,10 +4,10 @@ const observer = new MutationObserver(() => {
     console.log("yt-popup-container got a child element");
     observer.disconnect();
     removeAllChildNodes(elementToObserve);
-    console.log("removed all children from yt-popup-container");
+    console.log("Removed all children from yt-popup-container");
     // todo: emulate click required?
     this.activateObserver();
-    console.log("observer is active again");
+    console.log("Observer is watching again!");
 });
 
 this.activateObserver();
